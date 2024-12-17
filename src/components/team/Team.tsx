98616af3592bcabb891cb26dd4a063a7ab0acd77
Abhishek <guptaknights1820@gmail.com>
2024-12-17 18:22:22 +0530

    some tweaks made

--- src/components/team/Team.tsx
+++ src/components/team/Team.tsx
@@ -117,21 +117,9 @@ const Team = () => {
       </motion.div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:grid-cols-2 gap-8 md:px-12 lg:px-20 px-2">
         {teamMembers.map((member, index) => (
-          <motion.div
-            initial="hidden"
-            whileInView="visible"
-            whileHover={{ scale: 1.1 }}
-            viewport={{ once: true }}
-            transition={{
-              duration: 0.1,
-              // delay: 0.4,
-            }}
-            variants={{
-              hidden: { y: 40 },
-              visible: { y: 0 },
-            }}
+          <div
             key={index}
-            className="p-6  rounded-lg text-center transform hover:scale-110 transition duration-300  shadow shadow-dark-40"
+            className="p-6 group cursor-pointer  rounded-lg text-center transform transition duration-300  shadow shadow-dark-40 hover:shadow hover:shadow-dark-10"
             style={{
               background:
                 "radial-gradient(94.79997781055154% 94.79999776367805% at 6.367042537459778% 5.199996515206351%, var(--token-b5d64e1c-d37d-4055-8848-8c028ccb4878, rgb(25, 22, 36))  0%, var(--token-6e1dd630-cd02-44c9-b21a-a542833cf79b, rgb(10, 9, 13))  100%)",
@@ -141,7 +129,7 @@ const Team = () => {
               <img
                 src={member.imageUrl}
                 alt={member.name}
-                className="w-80 h-72 object-contain rounded-lg mx-auto mb-10"
+                className="w-80 h-72 object-contain rounded-lg mx-auto mb-10 group-hover:scale-110 transition duration-300"
               />
               <p className="bg-dark-50 text-neutral-20 text-sm px-4 py-1 rounded-full mb-6">
                 {member.role}
@@ -151,7 +139,7 @@ const Team = () => {
               </h3>
               <p className="text-sm text-neutral-40 text-left">{member.background}</p>
             </div>
-          </motion.div>
+          </div>
         ))}
       </div>
     </div>
